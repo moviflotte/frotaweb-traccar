15baff15240bf811eada0fc14e09f777544b909f
jcardus <joaquim_cardeira@hotmail.com>
2024-11-03 17:08:57 +0000

    fix patch

--- prebuild.sh
+++ prebuild.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone --depth 1 https://github.com/jcardus/traccar-web || true
-cp -vr traccar-patch/* traccar-web
+cp -vr src/* traccar-web/src
+cp -v vite.config.js traccar-web
 
 if [ -n "${LOGO_URL}" ]; then
   curl "${LOGO_URL}" > traccar-web/public/logo.svg
