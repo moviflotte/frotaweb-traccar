3256a4c5bcba7f31218c1af5c08dca413a3de17e
jcardus <joaquim_cardeira@hotmail.com>
2024-11-06 23:35:16 +0000

    bigger font and bold

--- prebuild.sh
+++ prebuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git clone --depth 1 https://github.com/jcardus/traccar-web || true
+find traccar-web/src -type f -name "*.js" -exec sed -i "s/'text-size': 12,/'text-size': 14,/g" {} +
+find traccar-web/src -type f -name "*.js" -exec sed -i "s/Noto Sans Regular/Noto Sans Bold/g" {} +
 cp -vr src/* traccar-web/src
 cp -v vite.config.js traccar-web
 
