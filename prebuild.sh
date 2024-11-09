5ebd5eaf11dc9c1e4ae7034a58250c8efd27daf4
jcardus <joaquim_cardeira@hotmail.com>
2024-11-09 15:07:35 +0000

    remove index.jsx simplify sed 1.2

--- prebuild.sh
+++ prebuild.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 git clone --depth 1 https://github.com/jcardus/traccar-web || true
 find traccar-web/src -type f -name "*.js" -exec sed -i "s/'text-size': 12,/'text-size': 14,/g" {} +
-find traccar-web/src -type f -name "*.js" -exec sed -i "s/Noto Sans Regular/Noto Sans Bold/g" {} +
-find traccar-web/src/settings -type f -name "SharePage.jsx" -exec sed -i "s|{window.location.origin}|{window.location.origin}/traccar|" {} +
+sed -i "s/Noto Sans Regular/Noto Sans Bold/g" traccar-web/src/map/core/mapUtil.js
+sed -i "s|{window.location.origin}|{window.location.origin}/traccar|" traccar-web/src/settings/SharePage.jsx
+sed -i "s|<BrowserRouter>|<BrowserRouter basename=\"/traccar\">|" traccar-web/src/index.jsx
+
+
 cp -vr src/* traccar-web/src
 cp -v vite.config.js traccar-web
 
