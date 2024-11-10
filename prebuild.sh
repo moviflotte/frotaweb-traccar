2c6eceea5e9d4f65073e1b27ecebfcb93b758415
jcardus <asklocation.net@gmail.com>
2024-11-10 17:51:15 +0000

    maybe doesn't work on linux

--- prebuild.sh
+++ prebuild.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git clone --depth 1 https://github.com/jcardus/traccar-web || true
-find traccar-web/src -type f -name "*.js" -exec sed -i '' "s/'text-size': 12,/'text-size': 14,/g" {} +
-sed -i '' "s/Noto Sans Regular/Noto Sans Bold/g" traccar-web/src/map/core/mapUtil.js
-sed -i '' "s|{window.location.origin}|{window.location.origin}/traccar|" traccar-web/src/settings/SharePage.jsx
-sed -i '' "s|<BrowserRouter>|<BrowserRouter basename=\"/traccar\">|" traccar-web/src/index.jsx
+find traccar-web/src -type f -name "*.js" -exec sed -i  "s/'text-size': 12,/'text-size': 14,/g" {} +
+sed -i  "s/Noto Sans Regular/Noto Sans Bold/g" traccar-web/src/map/core/mapUtil.js
+sed -i  "s|{window.location.origin}|{window.location.origin}/traccar|" traccar-web/src/settings/SharePage.jsx
+sed -i  "s|<BrowserRouter>|<BrowserRouter basename=\"/traccar\">|" traccar-web/src/index.jsx
 
 cp -vr src/* traccar-web/src
 cp -v vite.config.js traccar-web
