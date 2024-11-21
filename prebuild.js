af7ba29e55d1e708e2418b5cf4e5783edde86151
jcardus <asklocation.net@gmail.com>
2024-11-21 18:34:50 +0000

    update version

--- prebuild.js
+++ prebuild.js
@@ -12,9 +12,17 @@ files.forEach((file) => {
     if (fs.existsSync(file)) {
         const data = JSON.parse(fs.readFileSync(file, 'utf8'));
         data.mapOpenFreeMap = 'OpenFreeMap'; // Add new key-value pair
+        data.categoryTrailer = 'Rampa'; // Add new key-value pair
         fs.writeFileSync(file, JSON.stringify(data, null, 2), 'utf8');
         console.log(`Updated ${file}`);
     } else {
         console.error(`File not found: ${file}`);
     }
+    const packageJsonPath = './package.json';
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+    const versionParts = packageJson.version.split('.');
+    versionParts[2] = parseInt(versionParts[1], 10) + 1; // Increment the patch version
+    packageJson.version = versionParts.join('.');
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n', 'utf8');
+
 });
