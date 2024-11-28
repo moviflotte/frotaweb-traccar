7be603c3437fa762f9870847e982cb4148de4eda
jcardus <joaquim_cardeira@hotmail.com>
2024-11-28 17:54:54 +0000

    deploy also in root

---
+++ functions/icons3d/[[icons3d]].js
@@ -0,0 +1,30 @@
+export const onRequest = async ({request, env}) => {
+    if (!env.ICONS_3D_BASE_URL) { return new Response('not found', {status: 400}) }
+    const url = new URL(request.url);
+    const rewrittenUrl = new URL(`${env.ICONS_3D_BASE_URL}/${url.pathname.replace("/icons3d/", "")}`);
+
+    rewrittenUrl.search = url.search;
+
+    const newRequest = new Request(rewrittenUrl, request);
+
+    const cache = caches.default;
+    let response = await cache.match(newRequest);
+
+    if (!response) {
+        console.log('cache miss', rewrittenUrl)
+        response = await fetch(newRequest);
+        const headers = new Headers(response.headers);
+        headers.set('Cache-Control', 'max-age=31536000, public, immutable');
+
+        response = new Response(response.body, {
+            status: response.status,
+            statusText: response.statusText,
+            headers,
+        });
+        await cache.put(newRequest, response.clone());
+    } else {
+        console.log('cache hit', rewrittenUrl)
+    }
+
+    return response
+}
--- postbuild.sh
+++ postbuild.sh
@@ -5,4 +5,5 @@ tail traccar-web/build/sw.js
 mkdir -p deploy
 mv -v traccar-web/build deploy
 mv -v deploy/build deploy/traccar
-cp -v deploy/traccar/index.html deploy/traccar/404.html
+cp -vr deploy/build/* deploy
+cp -vv deploy/traccar/index.html deploy/traccar/404.html
