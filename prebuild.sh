9bd26ce2138532fb292b740f6467741bf439bbb5
jcardus <asklocation.net@gmail.com>
2024-11-29 14:57:41 +0000

    ups

--- prebuild.sh
+++ prebuild.sh
@@ -2,11 +2,6 @@
 GIT_TRACE=1 git clone --depth 1 https://github.com/traccar/traccar-web || true
 curl https://raw.githubusercontent.com/entrack-plataforma/frotaweb/refs/heads/main/src/theme/palette.js > traccar-web/src/common/theme/palette.js
 
-{
-  echo "import './sentry.js';"
-  cat traccar-web/src/index.jsx
-} > temp && mv temp traccar-web/src/index.jsx
-
 node prebuild.js
 
 cp -vr src/* traccar-web/src
