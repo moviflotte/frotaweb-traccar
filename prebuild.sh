095faa08f6d26ee606b7b1c7c3aed72433d615a4
jcardus <asklocation.net@gmail.com>
2024-12-02 09:41:29 +0000

    110

--- prebuild.sh
+++ prebuild.sh
@@ -2,7 +2,7 @@
 GIT_TRACE=1 git clone --depth 1 https://github.com/traccar/traccar-web || true
 curl https://raw.githubusercontent.com/entrack-plataforma/frotaweb/refs/heads/main/src/theme/palette.js > traccar-web/src/common/theme/palette.js
 
-perl -pi -e "s|'text-field': '▲',|'text-field': '▲','text-size': 17,|g" traccar-web/src/map/MapRoutePoints.js
+perl -pi -e "s|'text-field': '▲',|'text-field': '▲','text-size': 15,|g" traccar-web/src/map/MapRoutePoints.js
 perl -pi -e "s|'text-color':\s*\[\s*'get'\s*,\s*'color'\s*\],| 'text-color': ['get', 'color'],'text-halo-width': 1,'text-halo-color': '#2d5f99',|" traccar-web/src/map/MapRoutePoints.js
 perl -pi -e "s|'line-width': 2,|'line-width': 6,|g" traccar-web/src/map/MapRoutePath.js
 
