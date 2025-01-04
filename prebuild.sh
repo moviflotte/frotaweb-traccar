c9f9c320c52d85c379f4ffa17849db87584b4395
jcardus <asklocation.net@gmail.com>
2025-01-04 19:23:46 +0000

    log description

--- prebuild.sh
+++ prebuild.sh
@@ -22,6 +22,7 @@ fi
 FILES=("vite.config.js" "traccar-web/vite.config.js" "traccar-web/index.html")
 for FILE in "${FILES[@]}"; do
     echo "changing title to $TITLE $npm_package_version in $FILE"
+    echo "changing description to $DESCRIPTION in $FILE"
     sed -i "s|\${title}|$TITLE $npm_package_version|g" "$FILE" || true
     sed -i "s|\${name}|$NAME|g" "$FILE" || true
     sed -i "s|\${description}|$DESCRIPTION|g" "$FILE" || true
