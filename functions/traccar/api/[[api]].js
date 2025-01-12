39fee64886f1541d891183de972f42c565a0febc
jcardus <asklocation.net@gmail.com>
2025-01-12 20:05:02 +0000

    Cannot read properties of undefined (reading 'replace')

--- functions/traccar/api/[[api]].js
+++ functions/traccar/api/[[api]].js
@@ -1,8 +1,7 @@
 export const onRequest = ({request, env}, cf) => {
-    const url = new URL(request.url)
+    const url = new URL(request.url.replace('/traccar', '/'))
     url.host = env.TRACCAR_SERVER || 'gps.frotaweb.com'
     url.protocol = 'http:'
     url.port = 80
-    url.pathname = request.url.pathname.replace('/traccar', '/')
     return fetch(new Request(url, request), cf)
 }
