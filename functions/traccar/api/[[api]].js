2699bcb8ff962a2e289237d524fb0d355d1b066b
jcardus <asklocation.net@gmail.com>
2025-01-12 20:14:07 +0000

    simplify

--- functions/traccar/api/[[api]].js
+++ functions/traccar/api/[[api]].js
@@ -1,7 +1,5 @@
 export const onRequest = ({request, env}, cf) => {
-    const _url = request.url.replace('/traccar/', '/')
-    console.log(request.url, _url)
-    const url = new URL(_url)
+    const url = new URL(request.url.replace('/traccar/', '/'))
     url.host = env.TRACCAR_SERVER || 'gps.frotaweb.com'
     url.protocol = 'http:'
     url.port = 80
