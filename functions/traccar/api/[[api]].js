bfb93dc8ab506c1209a8d8b9ef01772d70b47765
jcardus <asklocation.net@gmail.com>
2025-01-12 20:12:16 +0000

    reason: Ambiguous URI empty segment

--- functions/traccar/api/[[api]].js
+++ functions/traccar/api/[[api]].js
@@ -1,5 +1,5 @@
 export const onRequest = ({request, env}, cf) => {
-    const _url = request.url.replace('/traccar', '/')
+    const _url = request.url.replace('/traccar/', '/')
     console.log(request.url, _url)
     const url = new URL(_url)
     url.host = env.TRACCAR_SERVER || 'gps.frotaweb.com'
