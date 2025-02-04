60ff19d027174d905e4ddc664312c3630ec9cdf5
moviflotte <136280595+moviflotte@users.noreply.github.com>
2025-02-04 17:23:04 +0000

    log

--- functions/api/positions/[[api]].js
+++ functions/api/positions/[[api]].js
@@ -1,5 +1,7 @@
 export const onRequest = ({request, env}, cf) => {
     const url = new URL(request.url)
+    console.log('positions')
+    console.log(request.url)
     url.host = 'kt24ptntaq46xbiqh5acvawdge0fdjzu.lambda-url.us-east-1.on.aws'
     return fetch(new Request(url, request), cf)
 }
