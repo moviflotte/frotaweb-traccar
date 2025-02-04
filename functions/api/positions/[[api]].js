69e098149616a0c438f238564c410ffcf2b59252
moviflotte <136280595+moviflotte@users.noreply.github.com>
2025-02-04 17:54:54 +0000

    fix url

--- functions/api/positions/[[api]].js
+++ functions/api/positions/[[api]].js
@@ -1,7 +1,5 @@
 export const onRequest = ({request, env}, cf) => {
     const url = new URL(request.url)
-    console.log('positions')
-    console.log(request.url)
-    url.host = 'kt24ptntaq46xbiqh5acvawdge0fdjzu.lambda-url.us-east-1.on.aws'
+    url.host = 'ltqgfyvcklxzaonv7h4rlmghai0rszop.lambda-url.us-east-1.on.aws'
     return fetch(new Request(url, request), cf)
 }
