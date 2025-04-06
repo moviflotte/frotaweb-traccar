9fb6a758663d5f1a0a255af0645ed68205b21d9a
jcardus <asklocation.net@gmail.com>
2025-04-07 00:28:07 +0100

    log version

--- build-odoo.sh
+++ build-odoo.sh
@@ -32,4 +32,5 @@ cp -vr odoo/* traccar-web
 
 cd traccar-web || exit
 npm install @sentry/vite-plugin @sentry/react maplibregl-mapbox-request-transformer
+echo "building with version $$npm_package_version"
 export VITE_APP_VERSION=$npm_package_version && npm run build
