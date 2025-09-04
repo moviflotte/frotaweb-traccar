c44a8c04a873fd908d3687fe1839b27630ef3dd2
joaquimcardeira <joaquim.cardeira@gmail.com>
2025-09-04 20:29:35 +0100

    icons 3d in device list

--- src/map/core/icons3d/index.js
+++ src/map/core/icons3d/index.js
@@ -6,9 +6,10 @@ import {mapIcons} from "../preloadImages";
 import backgroundSvg from "../../../resources/images/background.svg";
 const baseUrl = '/icons3d'
 import createPalette from '@mui/material/styles/createPalette';
+import {getStatusColor} from "../../../common/util/formatter";
 
 export const icons = {
-    default: car,
+    default: truck,
     car,
     truck
 }
@@ -41,6 +42,10 @@ const imageCache = {}
 
 const mapPalette = createPalette({});
 
+export function getColor(color) {
+    return color === 'neutral' ? '#FE795D' : mapPalette[color] && mapPalette[color].main;
+}
+
 export default async (e, addToMap = true) => {
     if (!/^[a-z]+[0-9]?-[a-z]+-[0-9.]+$/.test(e.id)) {
         console.log('ignoring image', e.id)
@@ -51,7 +56,7 @@ export default async (e, addToMap = true) => {
     }
     if (!imageCache[e.id]) {
         const [category, color, rotation] = e.id.split('-')
-        const _color = color === 'neutral' ? '#FE795D' : mapPalette[color] &&  mapPalette[color].main
+        const _color = getColor(color);
         if (icons[category]) {
             const svg = icons[category](rotation, _color, mapPalette);
             const svgBlob = new Blob([svg], {type: 'image/svg+xml;charset=utf-8'});
@@ -72,4 +77,19 @@ export default async (e, addToMap = true) => {
         })
     }
     return imageCache[e.id]
+}
+
+export function getSVGIcon(device, position) {
+    const category = device.category || 'default'
+    const color = getColor(getStatusColor(device.status))
+    const rotation = position && (position.course - position.course % 22.5) || 0
+    if (icons[category]) {
+        return icons[category](rotation, color)
+            .replace(/\.cls-(\d+)/g, `.cls-$1-${device.id}`)
+            .replace(/class="cls-(\d+)"/g, `class="cls-$1-${device.id}"`)
+    } else if (iconsRemote[category]) {
+        const c = color.replace('#', '')
+        const url = `${baseUrl}/${iconsRemote[category]}.php?grados=${rotation}&a=${c}&b=${c}&c=${c}`
+        return `<img src="${url}" width="60" height="60" alt="${category}" />`
+    }
 }
