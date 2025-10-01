8c27eb418a5238ac6580543ea0e49ccc5262bf11
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2025-10-01 19:47:53 +0100

    retention weeks

--- functions/api/reports/[report].js
+++ functions/api/reports/[report].js
@@ -1,8 +1,7 @@
 export const onRequest = ({request, env}, cf) => {
     const url = new URL(request.url)
-    const oldest = new Date()
-    oldest.setMonth(oldest.getMonth() - parseInt(env.DATABASE_RETENTION_MONTHS || '2'))
-    const forward = url.searchParams.get('from') && new Date(url.searchParams.get('from')) < oldest
+    const oldest = new Date().setDate(new Date().getDate() - parseInt(env.DATABASE_RETENTION_WEEKS || '3') * 7)
+    const forward = url.searchParams.get('from') && new Date(url.searchParams.get('from')) < new Date(oldest)
     url.host =  forward?
         (env.TRACCAR_REPORTS_SERVER || 'aadobrygc6wsyawaleatkimjjm0cczwu.lambda-url.us-east-1.on.aws') :
         (env.TRACCAR_SERVER || 'gps.frotaweb.com')
