ad3872859e27a9f9f3eb9773ec07bbbc352825e5
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2025-11-02 01:53:39 +0000

    2 weeks retention

--- functions/api/reports/[report].js
+++ functions/api/reports/[report].js
@@ -1,6 +1,6 @@
 export const onRequest = ({request, env}, cf) => {
     const url = new URL(request.url.replace('/traccar/', '/'))
-    const oldest = new Date().setDate(new Date().getDate() - parseInt(env.DATABASE_RETENTION_WEEKS || '3') * 7)
+    const oldest = new Date().setDate(new Date().getDate() - parseInt(env.DATABASE_RETENTION_WEEKS || '2') * 7)
     const forward = url.searchParams.get('from') && new Date(url.searchParams.get('from')) < new Date(oldest)
     url.host =  forward?
         (env.TRACCAR_REPORTS_SERVER || 'aadobrygc6wsyawaleatkimjjm0cczwu.lambda-url.us-east-1.on.aws') :
