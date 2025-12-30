6fd4a93b9734b0a6ca83741e0c257683f9433c0b
moviflotte <136280595+moviflotte@users.noreply.github.com>
2025-12-30 17:49:21 +0000

    reports

--- functions/api/reports/[[api]].js
+++ functions/api/reports/[[api]].js
@@ -1,5 +1,5 @@
 export const onRequest = ({request, env}, cf) => {
-    const url = new URL(request.url)
+    const url = new URL(request.url.replace('/traccar/', '/'))
     url.host = 'ltqgfyvcklxzaonv7h4rlmghai0rszop.lambda-url.us-east-1.on.aws'
     return fetch(new Request(url, request), cf)
 }
